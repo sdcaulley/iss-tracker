4497c8fb103db7304aa6369d26933966b635e797
sdcaulley <stephanie.caulley@gmail.com>
2016-08-11 19:28:03 -0700

    grab function for lag lng

--- js/modal.js
+++ js/modal.js
@@ -20,6 +20,5 @@ span.onclick = function() {
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
     }
 }
--- js/test.js
+++ js/test.js
@@ -18,9 +18,31 @@ var geocoder;
         var marker = new google.maps.Marker({
             map: map,
             position: results[0].geometry.location
-        });
+        })
       } else {
         alert('Geocode was not successful for the following reason: ' + status);
       }
     });
   }
+
+function closeModal () {
+  modal.style.display = "none"
+}
+
+var close = document.getElementById('modalButton')
+close.addEventListener('click', closeModal)
+
+// grab lat lng from user submit
+function grabLatLng() {
+  var address = document.getElementById('address').value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == 'OK') {
+      alert(results[0].geometry.location)
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+var latLngFunc = document.getElementById('modalButton')
+latLngFunc.addEventListener('click', grabLatLng)
