e09ee76aebdf4f864e179dbeb294c250568c2479
Waxhoya <waxhoya@gmail.com>
2016-08-11 18:26:53 -0700

    Astronaut contructor and prototypes created

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 // start
+function Astronaut(name, craft) {
+	this.name = name;
+	this.craft = craft;
+	this.image = "";
+	this.biography = ;
+}
+
+Astronaut.prototype.addBio(biography) {
+	this.biography = biography;
+}
+
+Astronaut.prototype.addPicture(imgLink) {
+	this.image = imgLink;
+}
